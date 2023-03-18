ec5a7fc0ae210780f43be29462b0b67173da8855
Μustafa ÜΝΑL <52699858+muungas@users.noreply.github.com>
2023-03-18 22:28:20 +0100

    Create week3_homework_Mustafa.py

---
+++ week3_homework_Mustafa.py
@@ -0,0 +1,21 @@
+# Soru-1:
+
+# listem = [2,12, 9, 50, 7, 66,91,14, 143,23, 48, 19,100,71,28]
+
+# Yukarida verilen liste ile ilgili asagidaki sorulari yanitlayiniz.
+
+# 1-a: Kullanicidan bir sayi girmesini isteyiniz,
+# 1-b: Bu sayiyi verilen listenin ilk elemani olacak sekilde listeye ekleyiniz.
+# 1-c: Olusan yeni listenin icindeki tekli sayilari listeden cikarip tekli_sayilar adinda yeni bir liste olusturunuz.
+# 1-d: Her iki listeyi buyukten kucuge olacak sekilde siralayip ekrana yazdiriniz.
+
+
+# Soru-2:
+
+# listem2 = ["Ali", "Veli", ["Ayşe", "Nazan", "Zeynep"], 34, 65, 33, 5.6,"elma","Hollanda"]
+
+# 2-a: Verilen listenin icerisindeki "Nazan" elemanina ulasip print ediniz.
+# 2-b: Listenin son elemanina ulasip, print ediniz.
+# 2-c: Verilen listenin icerisindeki 34, 65, 33, 5.6 elemanlarina erisip, bu elemanlari yeni bir liste yapiniz.
+# 2-d: Yeni listenin eleman sayisini yazdiriniz.
+# 2-e: List Comprehensions (liste uretecleri) metodu ile, 10 elemanli bir liste olusturunuz.
